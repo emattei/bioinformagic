dafca41618322e6c036e4a22d3412e1e59e9ea78
Eugenio Mattei <emattei.phd@gmail.com>
2021-04-14 19:01:41 -0400

    - Backbone for the GEO downloader

---
+++ bash/geo-downloader.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+# Make sure that the script will exit in case of error.
+set -Eeuo pipefail
+
+# Call the cleanup function in case an error or interruptions occurs.
+trap cleanup SIGINT SIGTERM ERR
+
+# Call the cleanup function when the script ended successfully.
+#trap safe_cleanup EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p] [-o output_folder] -g GSE
+
+The script takes in input a GEO id and download all teh fastqs associated with the to it. A metadata file describing the 
+data is also provided. The GEO id could be referred to a project a subproject or a single dataset.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-g, --geo       GEO id
+-p, --parallel  Number of cores to use. Be carefull to not kill your I/O. Default: 1
+-o, --output    Path to the output folder. Default: new folder in the current directory using the GEO id as name.
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  flag=0
+  param=''
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -f | --flag) flag=1 ;; # example flag
+    -p | --param) # example named parameter
+      param="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${geo-}" ]] && die "Missing required parameter: GEO id"
+  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+
+  return 0
+}
+
+parse_params "$@"
+setup_colors
+
+# script logic here
+
+msg "${RED}Read parameters:${NOFORMAT}"
+msg "- flag: ${flag}"
+msg "- param: ${param}"
+msg "- arguments: ${args[*]-}"
